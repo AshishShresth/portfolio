36ca1a575d3389bfa290d5359ded09027103fb4c
Ashish Shrestha <ashish11stha@gmail.com>
2024-06-12 21:50:11 +0545

    feat: adds webpack file loader configs

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,10 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack App',
+            title: 'Ashish Shrestha',
             filename: 'index.html',
             template: 'src/index.html',
+            favicon: 'src/assets/favicon.ico'
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
@@ -50,6 +51,18 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
         ],
     },
 }
