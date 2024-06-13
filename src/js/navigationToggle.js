16a938da62d3c96ece4e7ea9a2c10223aa930e12
Ashish Shrestha <ashish11stha@gmail.com>
2024-06-13 22:39:13 +0545

    refactor: navigation toggle

--- src/js/navigationToggle.js
+++ src/js/navigationToggle.js
@@ -2,16 +2,38 @@ function navigationToggle() {
     const navToggle = document.querySelector('.mobile-nav-toggle');
     const nav = document.getElementById('primary-navigation');
 
-    navToggle.addEventListener('click', () => {
-        const visibility = nav.getAttribute('data-visible');
-        if (visibility === 'false') {
-            nav.setAttribute('data-visible', true);
-            navToggle.setAttribute('aria-expanded', true);
+    // Clear existing click and keydown listeners before adding new ones
+    function clearPreviousListeners() {
+        document.removeEventListener('click', outsideClickListener);
+        document.removeEventListener('keydown', escapeKeyListener);
+    }
+
+    const toggleNav = () => {
+        const isVisible = nav.dataset.visible === 'true';
+        nav.dataset.visible = !isVisible;
+        navToggle.setAttribute('aria-expanded', !isVisible);
+
+        if (isVisible) {
+            clearPreviousListeners();
         } else {
-            nav.setAttribute('data-visible', false);
-            navToggle.setAttribute('aria-expanded', false);
+            document.addEventListener('click', outsideClickListener);
+            document.addEventListener('keydown', escapeKeyListener);
         }
-    });
+    };
+
+    navToggle.addEventListener('click', toggleNav);
+
+    const outsideClickListener = (event) => {
+        if (!nav.contains(event.target) && !navToggle.contains(event.target)) {
+            toggleNav();
+        }
+    };
+
+    const escapeKeyListener = (event) => {
+        if (event.key === 'Escape') {
+            toggleNav();
+        }
+    };
 }
 
 export default navigationToggle;
