9513c6564f1dad7739d65c3732edbe67bc0ba692
Ashish Shrestha <ashish11stha@gmail.com>
2024-06-13 22:22:14 +0545

    feat: adds mobile navigation trigger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style/main.scss';
+import navigationToggle from './js/navigationToggle';
+
+function isDOMReady(fn) {
+    if (typeof fn !== 'function') return;
+
+    if (document.readyState === 'complete') {
+        return fn();
+    }
+
+    document.addEventListener('DOMContentLoaded', fn, false);
+}
+
+isDOMReady(function () {
+    navigationToggle()
+})
---
+++ src/js/navigationToggle.js
@@ -0,0 +1,17 @@
+function navigationToggle() {
+    const navToggle = document.querySelector('.mobile-nav-toggle');
+    const nav = document.getElementById('primary-navigation');
+
+    navToggle.addEventListener('click', () => {
+        const visibility = nav.getAttribute('data-visible');
+        if (visibility === 'false') {
+            nav.setAttribute('data-visible', true);
+            navToggle.setAttribute('aria-expanded', true);
+        } else {
+            nav.setAttribute('data-visible', false);
+            navToggle.setAttribute('aria-expanded', false);
+        }
+    });
+}
+
+export default navigationToggle;
